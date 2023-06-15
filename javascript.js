3d7ef2f4c51463567d51f8e0cb57cddcab47fed1
brothong2000 <115859557+brothong2000@users.noreply.github.com>
2023-06-15 16:11:15 +0700

    hay88

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "60%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  // ------//////
+//   var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("menu").style.top = "0";
+//   } else {
+//     document.getElementById("menu").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
